a0ca436e002600b064d69a0336a3342201745fcd
Dadi Utenga <dadyutenga@yahoo.com>
2025-12-14 00:04:07 +0300

    fix: Remove unnecessary :show action from before_action in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authorize_admin!
-  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_user, only: [ :edit, :update, :destroy ]
 
   # GET /users
   def index
